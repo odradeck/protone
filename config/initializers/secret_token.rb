944f1b7fba7369c7bdcbcdb81c7b26376bcffb17
odradeck <odradeck@gmail.com>
2013-01-03 08:39:23 +0900

    Init for protone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protone::Application.config.secret_token = 'eeb77c8efdcdf18ff4cabff77561e8afbb49d3440651ff58c9817f612701fa6a1aedf6e4c7e32bc052c7a98deb290cb54ca6b01f3fc54edec7e449e7b98fc618'
